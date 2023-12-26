c20b6a428b72e190912b3d8dedfc2b07857ed574
Bunny <bunnycdy@gmail.com>
2023-12-26 20:32:27 +0545

    `CartController` added

--- app/Http/Controllers/Cart/CartController.php
+++ app/Http/Controllers/Cart/CartController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\Cart;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class CartController extends Controller
 {
-    //
+    public function viewCart() //: Response
+    {
+        $cart_items = auth()->user()->cart;
+        return Inertia::render('Cart', [
+            'cart_items' => $cart_items,
+        ]);
+    }
 }
